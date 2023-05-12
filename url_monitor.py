dd40ddd277e8b19be91a4fe44de0047d18a26cab
wwwhaoxu <53362740+wwwhaoxu@users.noreply.github.com>
2023-05-12 22:58:07 +0800

    Update url_monitor.py

--- url_monitor.py
+++ url_monitor.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf8')
 
 
 single_login = "https://yth.mvwchina.com/rest/studentInfo/selectList"
-values = {"account":"医视界医院","password":"000000"}
+values = {"account":"xx","password":"xxx"}
 jdata = json.dump(values)
 req = urllib2.Request(single_login,jdata)
 response = urllib2.urlopen(req)
